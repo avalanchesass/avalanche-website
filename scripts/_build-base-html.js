7aeca984f56841396a3d26120decc7b1b161f92a
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-10-15 21:29:48 +0200

    Add support for nested pages

--- scripts/_build-base-html.js
+++ scripts/_build-base-html.js
@@ -9,7 +9,15 @@ const pages = glob.sync(path.join(process.cwd(), `pages`, `**`, `*.hbs`));
 module.exports = (data) => {
   pages.forEach((page) => {
     const baseTemplate = fs.readFileSync(page, `utf8`);
-    const outputFile = path.join(process.cwd(), `dist`, `${path.parse(page).name}.html`);
+    const pathName = path.parse(page).name;
+    const subPath = path
+      .parse(page.replace(path.join(process.cwd(), `pages`, path.sep), ``))
+      .dir.split(path.sep);
+    const outputPath = [process.cwd(), `dist`, ...subPath];
+    if (pathName !== `index`) {
+      outputPath.push(pathName);
+    }
+    const outputFile = path.join(...outputPath, `index.html`);
     buildHtml(baseTemplate, data, outputFile);
   });
 };
