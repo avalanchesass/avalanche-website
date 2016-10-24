b024a1586cccfc4e935ae7b25f7fcce5d032451a
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-10-24 19:51:21 +0200

    Update the modularis stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,10 +3,6 @@ const declarationBlockPropertiesOrder = require('./.csscomb.json')['sort-order']
 module.exports = {
   extends: `stylelint-config-modularis`,
   rules: {
-    'at-rule-empty-line-before': ['always', {
-      except: ['blockless-group'],
-      ignore: ['after-comment', 'all-nested']
-    }],
     'declaration-block-properties-order': declarationBlockPropertiesOrder,
     'no-indistinguishable-colors': [true, { threshold: 1 }]
   }
