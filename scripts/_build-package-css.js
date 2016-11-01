198a57494e8a266a56062340104e4796fc2f2696
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-11-01 15:09:52 +0100

    Use the packages test.scss file instead of the index.scss file for generating the css for the package page

--- scripts/_build-package-css.js
+++ scripts/_build-package-css.js
@@ -4,7 +4,7 @@ const buildCss = require(`./_build-css.js`);
 
 module.exports = (packageName) => {
   const packagePath = path.join(process.cwd(), `avalanche`, `packages`, packageName);
-  const inputFile = path.join(packagePath, `scss`, `index.scss`);
+  const inputFile = path.join(packagePath, `test`, `test.scss`);
   const outputFile = path.join(process.cwd(), `dist`, `packages`, packageName, `css`, `index.css`);
 
   buildCss(inputFile, outputFile);
