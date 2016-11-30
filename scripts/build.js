e503dea58f44ac41fd18cbeaa634c6b373bb5b8d
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-11-30 19:30:09 +0100

    Clone the default page data for every package to prevent problems with async tasks

--- scripts/build.js
+++ scripts/build.js
@@ -6,21 +6,22 @@ const getDirectories = require(`./lib/get-directories.js`);
 
 const packages = getDirectories(`avalanche/packages`);
 
-const data = {
+const defaultData = {
   css: `<link rel="stylesheet" href="/base/css/global.css">`
 };
 
-buildBaseHtml(data);
+buildBaseHtml(defaultData);
 buildBaseCss();
 
 packages.forEach((packageName) => {
-  data.title = packageName;
+  const packageData = JSON.parse(JSON.stringify(defaultData));
+  packageData.title = packageName;
 
-  data.css = [
+  packageData.css = [
     `<link rel="stylesheet" href="/base/css/global.css">`,
     `<link rel="stylesheet" href="/packages/${packageName}/css/index.css">`
   ].join(`\n`);
 
-  buildPackageHtml(packageName, data);
+  buildPackageHtml(packageName, packageData);
   buildPackageCss(packageName);
 });
