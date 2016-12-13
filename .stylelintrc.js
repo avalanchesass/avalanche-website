70802e5a1e847949dbde8f8b07e6b9f7b7599ee9
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-12-13 15:00:42 +0100

    Remove csscomb because stylelint + stylefmt can do the same.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,6 @@
-const declarationBlockPropertiesOrder = require(`./.csscomb.json`)[`sort-order`][0];
-
 module.exports = {
   extends: `stylelint-config-modularis`,
   rules: {
-    'declaration-block-properties-order': declarationBlockPropertiesOrder,
     'no-indistinguishable-colors': [true, { threshold: 1 }],
     'string-no-newline': null,
   },
