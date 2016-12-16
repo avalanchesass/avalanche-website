c3cbf9b2c7da758c6af15b52b1e05d9366b90d51
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-12-16 10:51:41 +0100

    Fix linting error

--- scripts/lib/hbs2html.js
+++ scripts/lib/hbs2html.js
@@ -18,7 +18,7 @@ module.exports = (template, data, outputFile) => {
   if (minify) {
     uncss(html, { htmlroot: `dist` }, (error, output) => {
       const minifiedCss = new CleanCss({
-        semanticMerging: true
+        semanticMerging: true,
       }).minify(output).styles;
       // eslint-disable-next-line no-param-reassign
       data.css = `<style>${minifiedCss}</style>`;
