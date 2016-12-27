6e71cc67acf457269cda60e5b044286b7284b9f7
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-12-27 15:34:50 +0100

    Load the main scss file from packages to generate the styles for the demo code.

--- scripts/build/package-css.js
+++ scripts/build/package-css.js
@@ -4,7 +4,7 @@ const sass2css = require(`../lib/sass2css.js`);
 
 module.exports = (packageName) => {
   const packagePath = path.join(process.cwd(), `avalanche`, `packages`, packageName);
-  const inputFile = path.join(packagePath, `test`, `test.scss`);
+  const inputFile = path.join(packagePath, `scss`, `index.scss`);
   const outputFile = path.join(process.cwd(), `dist`, `packages`, packageName, `css`, `index.css`);
 
   sass2css(inputFile, outputFile, { cwd: packagePath });
